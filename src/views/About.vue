5efd56afe24fab1c4d2f0d230aa2e4c214deb0b7
unknown <fernandop@pmgintelligence.com>
2020-07-01 17:08:52 -0400

    about page created test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
+    <AboutComponent />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import AboutComponent from '@/components/AboutComponent.vue'
+
+export default {
+  name: 'About',
+  components: {
+    AboutComponent
+  }
+}
+
+</script>
